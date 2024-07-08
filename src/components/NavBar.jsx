4fc571ea0c5aef1f554493895693aa747a248e46
Emmanuel Ekpobimi <noble4special247@gmail.com>
2024-07-08 13:04:22 +0100

    Added store pictures and modified the navigation bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,89 @@
-import { BiCart } from "react-icons/bi";
-import { Link } from "react-router-dom";
+import { Navbar, Nav, Form, Button, Container, Image } from "react-bootstrap";
+import { BiCart, BiShoppingBag } from "react-icons/bi";
+import { NavLink } from "react-router-dom";
+import Logo from "../assets/logo.png";
+import { BsHeart } from "react-icons/bs";
+import Avatar from "../assets/avatar.png";
+import { RxAvatar } from "react-icons/rx";
 
 export default function NavBar() {
+  const iconStyles = {
+    fontSize: "20px",
+    border: "1px solid white",
+    borderRadius: "50%",
+    padding: "5px 10px",
+  };
+
   return (
-    <nav>
+    <Navbar
+      expand="lg"
+      className="bg-primary navbar-dark"
+    >
+      <Container>
+        <NavLink to="/">
+          <Image
+            width="100"
+            src={Logo}
+          />
+        </NavLink>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Form className="d-flex ms-auto">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          <Nav
+            className="ms-auto my-2 my-lg-0"
+            /*  style={{ maxHeight: "100px" }}
+            navbarScroll */
+          >
+            <NavLink
+              className="border-1 border-white rounded"
+              to="#"
+            >
+              <div
+                className="mx-3"
+                style={iconStyles}
+              >
+                <BsHeart />
+              </div>
+            </NavLink>
+            <NavLink to="/cart">
+              <div
+                className="mx-3"
+                style={iconStyles}
+              >
+                <BiShoppingBag />
+              </div>
+            </NavLink>
+            <NavLink href="#">
+              <div
+                className="mx-3"
+                style={iconStyles}
+              >
+                <RxAvatar />
+              </div>
+              {/* <img
+                width="30"
+                src={Avatar}
+                className="img-fluid rounded"
+                s
+                alt="profile-pic"
+              /> */}
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+/*  <nav>
       <ul>
         <li>
           <Link to="/">Shop</Link>
@@ -14,6 +94,104 @@ export default function NavBar() {
           </Link>
         </li>
       </ul>
+    </nav> */
+
+/* 
+    
+    <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
+      <div className="container">
+        <a
+          className="navbar-brand"
+          href="#"
+        >
+          <img
+            width="100"
+            src={Logo}
+            className="img-fluid rounded-top"
+            alt=""
+          />
+        </a>
+        <button
+          className="navbar-toggler d-lg-none"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapsibleNavId"
+          aria-controls="collapsibleNavId"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse"
+          id="collapsibleNavId"
+        >
+          <form className="d-flex my-2 my-lg-0 ms-auto">
+            <input
+              className="form-control me-sm-2"
+              type="text"
+              placeholder="Search"
+            />
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+          <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
+            <li className="nav-item">
+              <a
+                className="nav-link active"
+                href="#"
+                aria-current="page"
+              >
+                <BsHeart />
+                <span className="visually-hidden">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#"
+              >
+                <BiShoppingBag />
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#"
+              >
+                <img
+                  width="30"
+                  src={Avatar}
+                  className="img-fluid rounded"
+                  s
+                  alt="profile-pic"
+                />
+              </a>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="dropdownId"
+              >
+                <a
+                  className="dropdown-item"
+                  href="#"
+                >
+                  Action 1
+                </a>
+                <a
+                  className="dropdown-item"
+                  href="#"
+                >
+                  Action 2
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-  );
-}
+    
+    */
--- src/products.js
+++ src/products.js
@@ -1,59 +1,86 @@
-import product1 from "./assets/products/iphone.png";
-import product2 from "./assets/products/laptop.png";
-import product3 from "./assets/products/camera.png";
-import product4 from "./assets/products/long_sleeve.png";
-import product5 from "./assets/products/led_light.png";
-import product6 from "./assets/products/sweater.webp";
-import product7 from "./assets/products/short_sleeve.webp";
-import product8 from "./assets/products/facecap.webp";
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.png";
+import product8 from "./assets/products/8.png";
+import product9 from "./assets/products/9.png";
 
 export const shopProducts = [
   {
     id: 1,
-    productName: "IPhone",
-    price: 999.0,
+    productName: "Banana Boat Light As Air SPF 50+",
     productImage: product1,
+    price: 75,
+    stockInfo: "13,000 sold",
   },
   {
     id: 2,
-    productName: "Macbook Pro 2022 (M1)",
-    price: 1999.0,
+    productName: "Black Girl Sunscreen SPF 30",
     productImage: product2,
+    price: 75,
+    stockInfo: "13,000 sold",
   },
   {
     id: 3,
-    productName: "Cannon M50 Camera",
-    price: 699.0,
+    productName: "Supergoop Play Everyday Lotion SPF 50",
     productImage: product3,
+    price: 75,
+    stockInfo: "13,000 sold",
   },
   {
     id: 4,
-    productName: "WLS Van Gogh Denim Jacket",
-    price: 228.0,
+    productName: "Supergoop Unseen Sunscreen SPF 40",
     productImage: product4,
+    price: 75,
+    stockInfo: "13,000 sold",
   },
   {
     id: 5,
-    productName: "LED Light Strips",
-    price: 19.99,
+    productName: "Supergoop Unseen Sunscreen SPF 40",
     productImage: product5,
+    price: 75,
+    stockInfo: "13,000 sold",
   },
   {
     id: 6,
-    productName: "SPECTRUM LS TEE",
-    price: 68.0,
+    productName: "Trader Joe’s Daily Facial Sunscreen SPF 40",
     productImage: product6,
+    price: 75,
+    stockInfo: "13,000 sold",
   },
   {
     id: 7,
-    productName: "AUTO SERVICE SHIRT by GOLF WANG",
-    price: 120.0,
+    productName: "Trader Joe’s Daily Facial Sunscreen SPF 40",
     productImage: product7,
+    price: 75,
+    stockInfo: "13,000 sold",
   },
   {
     id: 8,
-    productName: "DON'T TRIP UNSTRUCTURED HAT",
-    price: 40.0,
+    productName: "TDr. Jart+ Mineral Sunscreen SPF 50+",
     productImage: product8,
+    price: 75,
+    stockInfo: "13,000 sold",
   },
+  {
+    id: 8,
+    productName: "Dr. Jart+ Mineral Sunscreen SPF 50+T",
+    productImage: product9,
+    price: 75,
+    stockInfo: "13,000 sold",
+  },
+];
+
+export const productCategories = [
+  { id: 1, categoryName: "All Products" },
+  { id: 2, categoryName: "Cleansers" },
+  { id: 3, categoryName: "Toners" },
+  { id: 4, categoryName: "'Moisturizers" },
+  { id: 5, categoryName: "Masks" },
+  { id: 6, categoryName: "Sunscreens" },
+  { id: 7, categoryName: "Exfoliators" },
+  { id: 8, categoryName: "Treatments" },
 ];
